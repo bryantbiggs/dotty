35ba70c9e67c4c44a376b2505d8bfbe1aaced359
Bryant Biggs <bryantbiggs@users.noreply.github.com>
2017-10-14 18:38:11 -0400

    update

--- main.sh
+++ main.sh
@@ -6,14 +6,14 @@ sudo -u $USER /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/H
 echo "Brew install"
 brew bundle
 
-echo "Dock - Remove all default app icons"
-defaults write com.apple.dock persistent-apps -array
-
-echo "Finder - Disable the warning when changing a file extension"
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-echo "Remove all app icons from the dock"
-defaults write com.apple.dock persistent-apps -array
+# echo "Dock - Remove all default app icons"
+# defaults write com.apple.dock persistent-apps -array
+#
+# echo "Finder - Disable the warning when changing a file extension"
+# defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+#
+# echo "Remove all app icons from the dock"
+# defaults write com.apple.dock persistent-apps -array
 
 echo "Install app store apps"
 mas signin --dialog bryantbiggs@gmail.com
@@ -26,19 +26,11 @@ mas install 512617038   # Snappy
 mas install 1176895641  # Spark
 mas upgrade
 
-echo "Add selected icons to dock"
-defaults write com.apple.dock persistent-apps -array-add'<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Launchpad.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-defaults write com.apple.dock persistent-apps -array-add'<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/App Store.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-defaults write com.apple.dock persistent-apps -array-add'<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Visual Studio Code.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-defaults write com.apple.dock persistent-apps -array-add'<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Firefox.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-defaults write com.apple.dock persistent-apps -array-add'<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Clear.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-defaults write com.apple.dock persistent-apps -array-add'<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-defaults write com.apple.dock persistent-apps -array-add'<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Atom.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-defaults write com.apple.dock persistent-apps -array-add'<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Bear.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-defaults write com.apple.dock persistent-apps -array-add'<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Spark.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-defaults write com.apple.dock persistent-apps -array-add'<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Slack.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-
-killall Dock
 
 echo "Update apps"
 brew update; brew upgrade; brew prune; brew cleanup; brew doctor
+
+# echo "Start docker and install codeclimate"
+# brew "codeclimate/formulae/codeclimate"
+
+source ./macos.sh
